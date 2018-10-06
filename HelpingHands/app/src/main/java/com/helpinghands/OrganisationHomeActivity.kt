0e3b51f8952eefd2c021f6c417c4b766f23fbf2b
Shuja Reshi <shujareshi1497@gmail.com>
2018-10-07 00:22:32 +0530

    home page of organisation created

---
+++ HelpingHands/app/src/main/java/com/helpinghands/OrganisationHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.helpinghands
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OrganisationHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_organisation_home)
+    }
+}
